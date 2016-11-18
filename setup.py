dc73f5987f31e1c5645d5cbe14ee642126f98ab4
Daniel Ireson <daniel@djgraphics.co.uk>
2016-11-18 12:34:08 +0000

    update setup.py to load from requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
 setup(
   name = 'facebook-bulk-group-inviter',
   version = '0.0.1',
   packages = ['cli'],
+  install_requires=requirements,
   entry_points = {
     'console_scripts': [
       'facebook-bulk-group-inviter = cli.__main__:main'
